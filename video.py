ace48d7d1d27db830219199b79bcb6fe9f4309b3
riersojw <jared.rierson@gmail.com>
2020-04-13 17:21:21 -0500

    Tracking red

--- video.py
+++ video.py
@@ -19,23 +19,30 @@ while (cap.isOpened()):
         bframe = cv2.GaussianBlur(frame, (7, 7), 1)
 	fgray = cv2.cvtColor(bframe, cv2.COLOR_BGR2GRAY)
 
-        lred = np.array([105, 150, 50])
-        ured = np.array([125, 255, 255])
-        mask = cv2.inRange(hsv, lred, ured)
+        lred = np.array([0, 50, 50])
+        ured = np.array([10, 255, 255])
+        mask0 = cv2.inRange(hsv, lred, ured)
+
+        lred = np.array([170, 50, 50])
+        ured = np.array([180, 255, 255])
+        mask1 = cv2.inRange(hsv, lred, ured)
+
+        mask = mask0 + mask1
 
         threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
         threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
         imgCanny = cv2.Canny(fgray, threshold1, threshold2)
+	kernel = np.ones((5, 5))
+	imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
         contours, hierarchy = cv2.findContours(mask, 1, 2)
-        ret, thresh = cv2.threshold(fgray, 127, 255, 0)
-        gcontours, ghierarchy = cv2.findContours(thresh, 1, 2)
         cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
 	cv2.imshow('Frame', frame)
         cv2.imshow("Mask", mask)
         cv2.imshow("Canny", imgCanny)
-        cv2.imshow("Gray", fgray)
+        cv2.imshow("imgDil", imgDil)
+        #cv2.imshow("Gray", fgray)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
